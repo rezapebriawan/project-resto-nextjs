ebd996f48b47041f62dbb297ec3dcb94bc003c4d
Reza Pebriawan M <102270074+rezapebriawan@users.noreply.github.com>
2024-12-14 20:16:13 +0700

    Update detailbook.js

--- src/pages/api/detailbook.js
+++ src/pages/api/detailbook.js
@@ -1,16 +1,1 @@
-import { Author1, Cover1 } from "../../../public"
 
-const detail = [
-    {
-        id: 1,
-        image: Cover1,
-        title: "Selena",
-        creator: "Tere Liye",
-        author: Author1,
-        name: "Reza Pebriawan M",
-        date: "1 week ago",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus vestibulum augue ut aliquet. Mauris ante ligula, facilisis sed ornare eu, lobortis in odio. Praesent convallis urna a lacus interdum ut hendrerit risus congue. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In at libero sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis. Integer fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta.",
-    },    
-]
-
-export default detail
